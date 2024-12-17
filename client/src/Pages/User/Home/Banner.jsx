cb9e243ab06bd901444c873ec4b2d4402f57fb30
Shilizen <shilizenkv@gmail.com>
2024-12-17 23:51:38 +0530

    banner responsive completed

--- client/src/Pages/User/Home/Banner.jsx
+++ client/src/Pages/User/Home/Banner.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import homemodel  from '../../../assets/homemodel.png'
 const Banner = () => {
   return (
-    <div className='flex bg-slate-100  w-full'>
-        <div className='flex flex-col items-center flex-grow justify-center'>
+    <div className='flex bg-slate-100 min-h-52 w-full'>
+        <div className='flex relative flex-col items-center flex-grow justify-center'>
             <div className='space-y-3 '>
-            <h2 className='text-2xl'>Classic Exclusive</h2>
-            <h1 className='font-medium text-4xl'>Women's Collection</h1>
-            <h2 className='text-2xl'>UPTO 40% OFF</h2>
+            <h2 className=' md:text-2xl'>Classic Exclusive</h2>
+            <h1 className='font-medium md:text-4xl'>Women's Collection</h1>
+            <h2 className='md:text-2xl'>UPTO 40% OFF</h2>
             <button className='bg-black text-white p-2 rounded-md text-sm px-4'>Shop Now </button>
             </div>
         </div>
-        <div className='w-1/2 '>
-            <img src={homemodel} alt="" />
+        <div className=' w-1/2'>
+            <img src={homemodel} alt="" className='w-full h-full object-cover'/>
         </div>
     </div>
   )
