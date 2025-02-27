9d67c643298509f2e64e97446f14bf4cb60a34f7
Shilizen <shilizenkv@gmail.com>
2025-02-27 11:12:03 +0530

    Api changed

--- client/src/Constants/Api.js
+++ client/src/Constants/Api.js
@@ -1,9 +1,19 @@
 
-export const BaseURL = "http://localhost:7000/api/"
+// export const BaseURL = "http://localhost:7000/api/"
+
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: "http://localhost:7000/api/",
+//   withCredentials: true,
+// });
+
+
+export const BaseURL = "https://krist-shopping-server.onrender.com/api/"
 
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:7000/api/",
+  baseURL: "https://krist-shopping-server.onrender.com/api/",
   withCredentials: true,
 });
